d97cce4453524a6b473c0b0029207e7eeb4b1934
Lv Jingxin <lv510987@163.com>
2023-07-13 09:05:46 +0800

    feat: 去除冗余代码

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@
  * @Author: Lv Jingxin lv510987@163.com
  * @Date: 2023-07-12 10:45:50
  * @LastEditors: Lv Jingxin lv510987@163.com
- * @LastEditTime: 2023-07-12 17:49:42
+ * @LastEditTime: 2023-07-13 09:05:26
  * @FilePath: /my-vue-app/src/main.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -20,5 +20,3 @@ app.use(router);
 app.use(ArcoVue);
 
 app.mount("#app");
-
-// createApp(App).use(router).use(ElementPlus).mount("#app");

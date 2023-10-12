21098c864f00a2971ab854aef3848fe3f1d357f1
Lv Jingxin <lv510987@163.com>
2023-10-12 17:00:55 +0800

    feat: ui

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Author: Lv Jingxin lv510987@163.com
+ * @Date: 2023-07-12 10:45:50
+ * @LastEditors: Lv Jingxin lv510987@163.com
+ * @LastEditTime: 2023-10-12 16:48:29
+ * @FilePath: /my-vue-app/src/App.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <script setup lang="ts"></script>
 
 <template>

eb6abe836070618571d9fe9233374e30d9b697c2
Lv Jingxin <lv510987@163.com>
2025-01-14 15:45:35 +0800

    打包构建

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
  * @Author: Lv Jingxin lv510987@163.com
  * @Date: 2023-07-12 10:45:50
  * @LastEditors: Lv Jingxin lv510987@163.com
- * @LastEditTime: 2023-07-12 14:08:55
+ * @LastEditTime: 2025-01-14 15:45:19
  * @FilePath: /my-vue-app/vite.config.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -53,5 +53,9 @@ export default defineConfig({
       //   rewrite: (path) => path.replace(/^\/yapi/, "/"),
       // },
     },
+  },
+  build: {
+    outDir: "dist",
+    sourcemap: true,
   },
 });

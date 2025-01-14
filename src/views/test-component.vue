e8c8c48ab85856482452951a8321d44af9032e53
Lv Jingxin <lv510987@163.com>
2025-01-14 11:19:54 +0800

    ci: 更换扩展mod

--- src/views/test-component.vue
+++ src/views/test-component.vue
@@ -2,7 +2,7 @@
  * @Author: Lv Jingxin lv510987@163.com
  * @Date: 2023-07-12 15:45:49
  * @LastEditors: Lv Jingxin lv510987@163.com
- * @LastEditTime: 2025-01-14 11:12:54
+ * @LastEditTime: 2025-01-14 11:19:10
  * @FilePath: /my-vue-app/src/views/test-component.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->

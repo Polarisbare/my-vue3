4962a7988b5acd7c42274ebb061ad84f6266eb64
Lv Jingxin <lv510987@163.com>
2025-01-14 10:53:01 +0800

    ci: eslint问题

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -2,7 +2,7 @@
  * @Author: Lv Jingxin lv510987@163.com
  * @Date: 2023-07-12 16:14:09
  * @LastEditors: Lv Jingxin lv510987@163.com
- * @LastEditTime: 2024-05-16 14:12:47
+ * @LastEditTime: 2025-01-14 10:50:35
  * @FilePath: /my-vue-app/src/views/home/home.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -14,7 +14,8 @@
 
 <script lang="ts" setup>
 // import Layout from "@/Layout/index.vue";
-import { userStore } from "@/store/modules/user";
+import { onMounted } from "vue";
+import { userStore } from "../../store/modules/user";
 const store = userStore();
 const onChangeName = (name: string) => {
   store.updateUserName(name ?? "李同学");

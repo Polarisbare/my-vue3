891dd49f7f36e88063186289c14c5386009bbd08
Lv Jingxin <lv510987@163.com>
2025-03-11 13:40:52 +0800

    feat: 打印

--- src/views/test-component.vue
+++ src/views/test-component.vue
@@ -2,7 +2,7 @@
  * @Author: Lv Jingxin lv510987@163.com
  * @Date: 2023-07-12 15:45:49
  * @LastEditors: Lv Jingxin lv510987@163.com
- * @LastEditTime: 2025-01-14 11:19:10
+ * @LastEditTime: 2025-03-11 13:40:34
  * @FilePath: /my-vue-app/src/views/test-component.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -21,7 +21,7 @@ onMounted(() => {
       }
     }
   }
-  console.log("=========>冒泡排序", arr.value);
+  console.log("=========>排序之后数组", arr.value);
 });
 </script>
 
